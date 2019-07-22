2071e6d77016fd0c96bc542120ea063c6450d20a
Jogchem Andre Oord <jogchemandre.oord@servicepartners.nl>
2019-07-22 14:22:36 +0200

    example not working urls

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,22 @@ const Index = () => (
     <div>
         <p>Hello Next.js</p>
 
-        <Link href="/services" passHref as="/services">
-            <a>Services</a>
+        <Link href="/services" passHref as="/diensten">
+            <a>Services (href=services, as=/diensten)</a>
         </Link>
-
+        <br />
+        <Link href="/diensten" passHref as="/diensten">
+            <a>Services (href=diensten, as=/diensten)</a>
+        </Link>
         <br />
 
-        <Link href="/services/[uid]" passHref as="/services/test">
+        <Link href="/services" passHref as="/services">
+            <a>Services (href=services, as=/services)</a>
+        </Link>
+
+        <hr />
+
+        <Link href="/services/[slug]" passHref as="/diensten/test">
             <a>Service details</a>
         </Link>
     </div>
