000f7b7bc7885b02e4c697f13aa92d43817e3685
Jogchem Andre Oord <jogchemandre.oord@servicepartners.nl>
2019-07-22 15:19:01 +0200

    additional tests

--- pages/services/details.js
+++ pages/services/details.js
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+
 const Index = () => (
     <div>
         <p>Service details (details.js)</p>
 
 
+        <Link href="/services" passHref as="/diensten">
+            <a>Services (href=services, as=/diensten) / (works on click, but fails on reload/refresh in live)</a>
+        </Link>
+
+        |
+
+        <Link href="/services/test" passHref as="/diensten/test">
+            <a>Service details (with /test)</a>
+        </Link>
     </div>
 );
 
